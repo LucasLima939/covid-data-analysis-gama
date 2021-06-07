0df4e3b53978d743891451713af11b70dd3341b9
Lucas Lima <lucaslima939@gmail.com>
2021-06-07 20:08:58 -0300

    ajustes metodo request

--- repositories/covid_repository.py
+++ repositories/covid_repository.py
@@ -1,10 +1,14 @@
 import requests
 
 class CovidRepository:
-    def getAllCountries():
+    def getAllCountries(self):
         res = requests.get('https://api.covid19api.com/countries')
         if res.status_code != 200:
             print(res)
             raise Exception
-        res.json()
+        countriesJson = res.json()
+        countryList = list()
+        for country in countriesJson:
+            countryList.append(country['Country'])
+        print(countryList)
         
